d5395ba3fd304191a63902849b11df3aa9acef78
akazemtarghi <44892116+akazemtarghi@users.noreply.github.com>
2020-05-26 22:36:38 +0300

    modified,26th may

--- GLCM_main.py
+++ GLCM_main.py
@@ -1,4 +1,4 @@
-from GLCM_utilities import GLCM_amir, Histo_AMir
+from GLCM_utilities import GLCM_amir, Histo_AMir, Gradient_amir, LAP_amir
 import cv2
 from Utilities import SplittingData
 import numpy as np
@@ -8,19 +8,67 @@ from sklearn.linear_model import LogisticRegression
 from Utilities import set_ultimate_seed
 import scipy.stats as stats
 import image_slicer
+import pandas as pd
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #print("Normalized confusion matrix")
+
+
+    #print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                horizontalalignment="center",
+                color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+    plt.tight_layout()
 
 
 def center_crop_amir(dir, new_width, new_height):
 
     im = cv2.imread(dir, cv2.IMREAD_GRAYSCALE)
-
     width, height = im.shape  # Get dimensions
 
     left = (width - new_width)//2
     top = (height - new_height)//2
     right = (width + new_width)//2
     bottom = (height + new_height)//2
+
+    cropped = im[top:bottom, left:right]
+
+
+    return cropped
+
+def upper_crop(dir, new_width, new_height):
+
+    im = cv2.imread(dir, cv2.IMREAD_GRAYSCALE)
+    width, height = im.shape  # Get dimensions
+
+    left = (width - new_width)//2
+    top = 0
+    right = (width + new_width)//2
+    bottom = new_height
 
     cropped = im[top:bottom, left:right]
 
@@ -36,21 +84,27 @@ ASM
 energy
 '''
 
-Angle = [0,  np.pi/2, np.pi/4, (3/4)*np.pi, np.pi]
-Distances1 = list(range(1, 31))
-windowsize_r = 4
-windowsize_c = 4
+mode = ['dissimilarity', 'correlation', 'contrast', 'homogeneity', 'ASM', 'energy']
+Angle = [0,  np.pi/2, np.pi/4, (3/4)*np.pi]
+Distances = [1, 2, 3]
 
-p = []
-O = []
+pv = []
+odds_r = []
+m = []
+s = []
+idx = 0
+df = pd.DataFrame(columns=['Parameter', 'No_OA', 'No_OA_SD',
+                           'OA', 'OA_SD', 'pvalue', 'ODDS_ratio'])
 
-for a in range(len(Distances1)):
-    Distances = Distances1[0:a+1].copy()
+for q in mode:
+
+    print(q)
 
     Csv_dir = 'C:/Users/Amir Kazemtarghi/Documents/MASTER THESIS/test 1/med.csv'
-    train, test = SplittingData(Csv_dir, all=False)
-
-    n = (len(train))
+    # train, test = SplittingData(Csv_dir, all=False)
+    data = pd.read_csv(Csv_dir)
+    data = data.drop(columns=['Unnamed: 0'])
+    n = (len(data))
     feature = []
     Feature_T = []
     labels = []
@@ -58,78 +112,78 @@ for a in range(len(Distances1)):
 
     for i in range(n):
 
-        input = train.loc[i]
+        input = data.loc[i]
         side, label, series, id = input['side'], input['Label'], input['SeriesDescription'], input['ParticipantID']
-        imagePath = 'C:/Users/Amir Kazemtarghi/Documents/MASTER THESIS/test 1/patches/standard/' \
-                    + str(label) + '/' + str(id) + '_' + str(side) + '_' + str(series) +  '.png'
+        imagePath = 'C:/Users/Amir Kazemtarghi/Documents/MASTER THESIS/ROIs/proportional_mri/' \
+                    + str(label) + '/' + str(id) + '_' + str(series) + '_' + str(side)+ '.png'
 
         new_width = 30
         new_height = 30
 
-        #resized_im = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+        resized_im = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+        #resized_im = center_crop_amir(imagePath, new_width, new_height)
 
-        windowsize_r = 15
-        windowsize_c = 15
-
+        windowsize_r = 29
+        windowsize_c = 29
         # Crop out the window and calculate the histogram
-        # temp = []
-        # for r in range(0, resized_im.shape[0] - windowsize_r, windowsize_r):
-        #     for c in range(0, resized_im.shape[1] - windowsize_c, windowsize_c):
-        #         window = resized_im[r:r + windowsize_r, c:c + windowsize_c]
-        #         Sk, Kr, Vr, Mn = Histo_AMir(window)
-        #         my_feature = np.asarray(Vr)
-        #         temp.append(my_feature)
+        temp = []
 
+        Sk = GLCM_amir(resized_im, Angle, Distances, metric=q)
 
+        Sk = np.asarray(Sk)
+        Sk = Sk.mean()
 
-        resized_im = center_crop_amir(imagePath, new_width, new_height)
-
-        feature = GLCM_amir(resized_im, Angle, Distances, metric='contrast')
-
-
-        my_f = np.asarray(feature)
         labels.append(label)
-        Feature_T.append(my_f.reshape(1, -1))
+        Feature_T.append(Sk)
 
 
     # model = svm.SVC(kernel='linear')
     # model.fit(Feature_T, labels)
-    aaa = np.concatenate(Feature_T, axis=0)
-    model = LogisticRegression(random_state=0).fit(aaa, labels)
+    aaa = np.asarray(Feature_T)
+
+
+    bbb = np.asarray(labels)
+    INDEX_no = np.where(bbb == 0)
+    INDEX_oa = np.where(bbb == 1)
+    print(aaa[INDEX_no[0]].mean())
+    print(aaa[INDEX_no[0]].std())
+
+    print(aaa[INDEX_oa[0]].mean())
+    print(aaa[INDEX_oa[0]].std())
+
+    model = LogisticRegression(penalty='none', class_weight='balanced', solver='newton-cg')
+    model.fit(aaa.reshape(-1, 1), bbb)
 
 
 
 
-    n = (len(test))
+    n = (len(data))
     results = np.zeros((n, 2))
 
     for i in range(n):
 
-        input = test.loc[i]
+        input = data.loc[i]
         side, label, series, id = input['side'], input['Label'], input['SeriesDescription'], input['ParticipantID']
-        imagePath = 'C:/Users/Amir Kazemtarghi/Documents/MASTER THESIS/test 1/patches/standard/' \
-                    + str(label) + '/' + str(id) + '_' + str(side) + '_' + str(series) +  '.png'
+        imagePath = 'C:/Users/Amir Kazemtarghi/Documents/MASTER THESIS/ROIs/proportional_mri/' \
+                    + str(label) + '/' + str(id) + '_' + str(series) + '_' + str(side) + '.png'
+        resized_im = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
 
-        #resized_im = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
-
-        resized_im = center_crop_amir(imagePath, new_width, new_height)
-
-
-
-        feature = GLCM_amir(resized_im, Angle, Distances, metric='contrast')
-        # temp = []
-        # for r in range(0, resized_im.shape[0] - windowsize_r, windowsize_r):
-        #     for c in range(0, resized_im.shape[1] - windowsize_c, windowsize_c):
-        #         window = resized_im[r:r + windowsize_r, c:c + windowsize_c]
-        #         Sk, Kr, Vr, Mn = Histo_AMir(window)
-        #         my_feature = np.asarray(Vr)
-        #         temp.append(my_feature)
-
-        f = np.asarray(feature)
+        # resized_im = center_crop_amir(imagePath, new_width, new_height)
+        # feature = GLCM_amir(resized_im, Angle, Distances, metric='dissimilarity')'
 
 
-        prediction = model.predict(f.reshape(1, -1))
+        Sk = GLCM_amir(resized_im, Angle, Distances, metric=q)
+
+        Sk = np.asarray(Sk)
+        Sk = Sk.mean()
+
+
+
+
+
+        prediction = model.predict(Sk.reshape(-1, 1))
         results[i][0] = prediction
+
         results[i][1] = label
 
 
@@ -143,58 +197,37 @@ for a in range(len(Distances1)):
 
     odds_ratio, pvalue = stats.fisher_exact(cm)
 
-    O = np.append(O, odds_ratio)
-    p = np.append(p, pvalue)
+    odds_r.append(odds_ratio)
+    pv.append(pvalue)
+
+    df = df.append({'Parameter': q,
+                    'No_OA': aaa[INDEX_no[0]].mean(),
+                    'No_OA_SD': aaa[INDEX_no[0]].std(),
+                    'OA': aaa[INDEX_oa[0]].mean(),
+                    'OA_SD': aaa[INDEX_oa[0]].std(),
+                    'pvalue': pvalue,
+                    'ODDS_ratio': odds_ratio}, ignore_index=True)
+
     print(odds_ratio)
     print(pvalue)
 
-    def plot_confusion_matrix(cm, classes,
-                              normalize=False,
-                              title='Confusion matrix',
-                              cmap=plt.cm.Blues):
-        """
-        This function prints and plots the confusion matrix.
-        Normalization can be applied by setting `normalize=True`.
-        """
-        if normalize:
-            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-            #print("Normalized confusion matrix")
+    plt.figure()
+
+    plot_confusion_matrix(cm, classes=['no oa', 'oa'],
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues)
+    plt.savefig('C:/Users/Amir Kazemtarghi/Desktop/New folder/'+'_CM_' + str(idx) + '.jpg', bbox_inches='tight')
+
+    plt.figure()
+
+    plot_confusion_matrix(cm, classes=['no oa', 'oa'],
+                          normalize=True,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues)
+    plt.savefig('C:/Users/Amir Kazemtarghi/Desktop/New folder/' + '_N_' + str(idx) + '.jpg', bbox_inches='tight')
+    idx = idx + 1
 
 
-        #print(cm)
 
-        plt.imshow(cm, interpolation='nearest', cmap=cmap)
-        plt.title(title)
-        plt.colorbar()
-        tick_marks = np.arange(len(classes))
-        plt.xticks(tick_marks, classes, rotation=45)
-        plt.yticks(tick_marks, classes)
-
-        fmt = '.2f' if normalize else 'd'
-        thresh = cm.max() / 2.
-        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            plt.text(j, i, format(cm[i, j], fmt),
-                    horizontalalignment="center",
-                    color="white" if cm[i, j] > thresh else "black")
-
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label')
-
-        plt.tight_layout()
-
-    # plt.figure()
-    #
-    # plot_confusion_matrix(cm, classes=['no oa', 'oa'],
-    #                       normalize=False,
-    #                       title='Confusion matrix',
-    #                       cmap=plt.cm.Blues)
-    #
-    # plt.figure()
-    #
-    # plot_confusion_matrix(cm, classes=['no oa', 'oa'],
-    #                       normalize=True,
-    #                       title='Confusion matrix',
-    #                       cmap=plt.cm.Blues)
-    #
-    #
-    #
+df.to_csv('C:/Users/Amir Kazemtarghi/Desktop/df_GLCM.csv')
